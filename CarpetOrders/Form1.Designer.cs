5ba591846dee1feb29756ebdac3ed936f0f8ae5d
DeadPixelz01 <deadpixelz01@gmail.com>
2018-02-07 21:26:16 +1100

    Initial commit

---
+++ CarpetOrders/Form1.Designer.cs
@@ -0,0 +1,357 @@
+﻿namespace CarpetOrders
+{
+	partial class FrmCarpetOrders
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtQuality = new System.Windows.Forms.TextBox();
+            this.txtWidth = new System.Windows.Forms.TextBox();
+            this.txtLength = new System.Windows.Forms.TextBox();
+            this.txtArea = new System.Windows.Forms.TextBox();
+            this.txtDiscount = new System.Windows.Forms.TextBox();
+            this.txtTotalCost = new System.Windows.Forms.TextBox();
+            this.txtCost = new System.Windows.Forms.TextBox();
+            this.chkDiscount = new System.Windows.Forms.CheckBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.txtDate = new System.Windows.Forms.TextBox();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.label13 = new System.Windows.Forms.Label();
+            this.txtCalName = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))));
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(148, 30);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Carpet Orders";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(13, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 21);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Name:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(13, 112);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(129, 21);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Quality Number: ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(13, 149);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(164, 21);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Room width (metres): ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(13, 188);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(165, 21);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Room length (metres):";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(13, 228);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(122, 21);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Trade discount ?";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(13, 418);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(105, 21);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "Area to cover:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(13, 439);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(48, 21);
+            this.label8.TabIndex = 12;
+            this.label8.Text = "( m² )";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(13, 544);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(74, 21);
+            this.label9.TabIndex = 14;
+            this.label9.Text = "Discount:";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(331, 418);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(44, 21);
+            this.label10.TabIndex = 16;
+            this.label10.Text = "Cost:";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(331, 544);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(77, 21);
+            this.label11.TabIndex = 18;
+            this.label11.Text = "Total cost:";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(74, 68);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 20);
+            this.txtName.TabIndex = 19;
+            // 
+            // txtQuality
+            // 
+            this.txtQuality.Location = new System.Drawing.Point(148, 113);
+            this.txtQuality.Name = "txtQuality";
+            this.txtQuality.Size = new System.Drawing.Size(100, 20);
+            this.txtQuality.TabIndex = 20;
+            // 
+            // txtWidth
+            // 
+            this.txtWidth.Location = new System.Drawing.Point(183, 150);
+            this.txtWidth.Name = "txtWidth";
+            this.txtWidth.Size = new System.Drawing.Size(100, 20);
+            this.txtWidth.TabIndex = 22;
+            // 
+            // txtLength
+            // 
+            this.txtLength.Location = new System.Drawing.Point(184, 191);
+            this.txtLength.Name = "txtLength";
+            this.txtLength.Size = new System.Drawing.Size(100, 20);
+            this.txtLength.TabIndex = 24;
+            // 
+            // txtArea
+            // 
+            this.txtArea.Location = new System.Drawing.Point(124, 419);
+            this.txtArea.Name = "txtArea";
+            this.txtArea.Size = new System.Drawing.Size(100, 20);
+            this.txtArea.TabIndex = 26;
+            // 
+            // txtDiscount
+            // 
+            this.txtDiscount.Location = new System.Drawing.Point(93, 545);
+            this.txtDiscount.Name = "txtDiscount";
+            this.txtDiscount.Size = new System.Drawing.Size(100, 20);
+            this.txtDiscount.TabIndex = 28;
+            // 
+            // txtTotalCost
+            // 
+            this.txtTotalCost.Location = new System.Drawing.Point(414, 544);
+            this.txtTotalCost.Name = "txtTotalCost";
+            this.txtTotalCost.Size = new System.Drawing.Size(100, 20);
+            this.txtTotalCost.TabIndex = 30;
+            // 
+            // txtCost
+            // 
+            this.txtCost.Location = new System.Drawing.Point(383, 418);
+            this.txtCost.Name = "txtCost";
+            this.txtCost.Size = new System.Drawing.Size(100, 20);
+            this.txtCost.TabIndex = 32;
+            // 
+            // chkDiscount
+            // 
+            this.chkDiscount.AutoSize = true;
+            this.chkDiscount.Location = new System.Drawing.Point(141, 234);
+            this.chkDiscount.Name = "chkDiscount";
+            this.chkDiscount.Size = new System.Drawing.Size(15, 14);
+            this.chkDiscount.TabIndex = 33;
+            this.chkDiscount.UseVisualStyleBackColor = true;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.Location = new System.Drawing.Point(331, 67);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(45, 21);
+            this.label12.TabIndex = 34;
+            this.label12.Text = "Date:";
+            // 
+            // txtDate
+            // 
+            this.txtDate.Location = new System.Drawing.Point(383, 67);
+            this.txtDate.Name = "txtDate";
+            this.txtDate.Size = new System.Drawing.Size(100, 20);
+            this.txtDate.TabIndex = 35;
+            this.txtDate.TextChanged += new System.EventHandler(this.txtDate_TextChanged);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(670, 12);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 36;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(212, 273);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(164, 40);
+            this.btnCalculate.TabIndex = 37;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label13.Location = new System.Drawing.Point(17, 363);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(49, 21);
+            this.label13.TabIndex = 38;
+            this.label13.Text = "Hello,";
+            // 
+            // txtCalName
+            // 
+            this.txtCalName.Location = new System.Drawing.Point(73, 363);
+            this.txtCalName.Name = "txtCalName";
+            this.txtCalName.Size = new System.Drawing.Size(100, 20);
+            this.txtCalName.TabIndex = 39;
+            // 
+            // FrmCarpetOrders
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(757, 608);
+            this.Controls.Add(this.txtCalName);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.txtDate);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.chkDiscount);
+            this.Controls.Add(this.txtCost);
+            this.Controls.Add(this.txtTotalCost);
+            this.Controls.Add(this.txtDiscount);
+            this.Controls.Add(this.txtArea);
+            this.Controls.Add(this.txtLength);
+            this.Controls.Add(this.txtWidth);
+            this.Controls.Add(this.txtQuality);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "FrmCarpetOrders";
+            this.Text = "Melbourne Carpet Orders";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.Label label7;
+		private System.Windows.Forms.Label label8;
+		private System.Windows.Forms.Label label9;
+		private System.Windows.Forms.Label label10;
+		private System.Windows.Forms.Label label11;
+		private System.Windows.Forms.TextBox txtName;
+		private System.Windows.Forms.TextBox txtQuality;
+		private System.Windows.Forms.TextBox txtWidth;
+		private System.Windows.Forms.TextBox txtLength;
+		private System.Windows.Forms.TextBox txtArea;
+		private System.Windows.Forms.TextBox txtDiscount;
+		private System.Windows.Forms.TextBox txtTotalCost;
+		private System.Windows.Forms.TextBox txtCost;
+		private System.Windows.Forms.CheckBox chkDiscount;
+		private System.Windows.Forms.Label label12;
+		private System.Windows.Forms.TextBox txtDate;
+		private System.Windows.Forms.Button btnExit;
+		private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.TextBox txtCalName;
+    }
+}
+
---
+++ CarpetOrders/Form1.cs
@@ -0,0 +1,105 @@
+﻿// Author: Alexander Smith
+// Date: 2/7/2018
+// Description: A small C# script that utilises a GUI to calculate the cost of carpet based off of the area required.
+
+// From my understanding, these are like the C# equivalent to Python imports.
+// I'd like to imagine this where developers could import/use 3rd party packages.
+using System;
+using System.Globalization;
+using System.Windows.Forms;
+
+namespace CarpetOrders
+{
+    public partial class FrmCarpetOrders : Form
+    {
+        public FrmCarpetOrders()
+        {
+            InitializeComponent();
+        }
+
+        // Date.
+        // This has been setup so that when the user goes to enter the date,
+        // the program will pick up on this and instantly fill the textbox with the system's current date.
+        // See line 26 ("dd/MM/yyyy") to change the format.
+        private void txtDate_TextChanged(object sender, EventArgs e)
+        {
+            txtDate.Text = DateTime.Now.ToString("dd/MM/yyyy");
+        }
+
+        // Calculation button.
+        // This button calculates and outputs values.
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            // Defining variables and data types.
+            // Linking them back to the form ('Form1.Designer.cs').
+            // 'const' is used here because strInvalidSelection's string value will never change.
+            const string strInvalidSelection = "Invalid selection! Please choose between Quality No. 1, 2, 3 or 4"; // String.
+            var strName = txtName.Text; // String (This was never used, so why did we ask for it!? I fixed this :p).
+            var intQualityNo = int.Parse(txtQuality.Text); // Integer.
+            decimal decRWidth = decimal.Parse(txtWidth.Text), decRLength = decimal.Parse(txtLength.Text); // Decimal.
+            var decRArea = decRWidth * decRLength; // Calculating the room's area (Lenght * Width = Area).
+            decimal decDiscount, decCost, decTotalCost; // Decimal.
+            var boolTradeDiscount = chkDiscount.Checked; // Boolean.
+
+            // Using Switches and calculating Cost.
+            // Since the user has to choose between quality 1-4, the variable 'intQualityNo' is subbed into the switch.
+            // For example if the user entered '1', the variable cost would equal their area multiplied by 100.
+            // -- Side note --
+            // I'm probably going to end up replacing this ugly looking switch in favour of a dictionary.
+            // Dictionaries work better and look cleaner, and the only reason I haven't moved to using one
+            // is because I have no idea how to they work in C#. I learned the basics overnight... That was a very long,
+            // painfull night...
+            switch (intQualityNo)
+            {
+                case 1:
+                    decCost = decRArea * 100; // multiply by 100.
+                    break;
+                case 2:
+                    decCost = decRArea * 70; // multiply by 70.
+                    break;
+                case 3:
+                    decCost = decRArea * 45; // multiply by 45.
+                    break;
+                case 4:
+                    decCost = decRArea * 30; // multiply by 30.
+                    break;
+                // If the user doesn't enter a valid option, it defaults to this error:
+                default:
+                    decCost = 0;
+                    Console.WriteLine(strInvalidSelection);
+                    break;
+            }
+
+            // Calculating a discount.
+            // If the user asked for a Trade discount, then...
+            if (boolTradeDiscount)
+            {
+                // Discount is equal to the cost, multiplied by 0.1
+                decDiscount = decCost * Convert.ToDecimal(0.1);
+            }
+            // If they didn't, then...
+            else
+            {
+                // The user is given a discount of $0.
+                decDiscount = 0;
+            }
+
+            // To calculate the new price, this line subtracts the discount from the cost.
+            decTotalCost = decCost - decDiscount;
+
+            // Converting back to strings.
+            // A lot of the variables have the data type of integer and decimal,
+            // so they need to be converted back into strings so that they can be displayed within a textbox.
+            txtCalName.Text = strName;
+            txtArea.Text = decRArea.ToString(CultureInfo.CurrentCulture);
+            txtCost.Text = decCost.ToString(CultureInfo.CurrentCulture);
+            txtDiscount.Text = decDiscount.ToString(CultureInfo.CurrentCulture);
+            txtTotalCost.Text = decTotalCost.ToString(CultureInfo.CurrentCulture);
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
